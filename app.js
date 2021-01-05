57b60d35c96562d8b028d717ebeb1b096d47ab84
MattRoger <mrogerphotography@gmail.com>
2021-01-05 17:57:13 -0500

    added button function to pizza menu

--- app.js
+++ app.js
@@ -1,67 +1,82 @@
-const menu= document.querySelector(".menu-insert");
-const pizzaForm= document.createElement("form");
-const toppings=["Pepperoni", "Sausage", "Olives", "Pineapple", "Bacon"]
+const menu = document.querySelector(".menu-insert");
+const pizzaMenuButton = document.querySelector(".pizzaMenuButton");
+const appsMenuButton = document.querySelector(".appsMenuButton")
 
-const PizzaSize =()=>{
-    const small = document.createElement("input");
-    small.type="radio";
-    small.value=10.99;
-    small.name="size"
-    const smLabel= document.createElement("label");
-    smLabel.textContent = `Small 10" $10.99`;
+const appetizers=[]
 
-    const med = document.createElement("input");
-    med.type="radio";
-    med.value=12.99;
-    med.name="size"
-    const medLabel= document.createElement("label");
-    medLabel.textContent = `Medium 14" $12.99`;
+const pizzaForm = document.createElement("form");
+const toppings = ["Pepperoni", "Sausage", "Olives", "Pineapple", "Bacon"];
 
-    const lg = document.createElement("input");
-    lg.type="radio";
-    lg.value=14.99;
-    lg.name="size"
-    const lgLabel= document.createElement("label");
-    lgLabel.textContent = `Larger 16" $14.99`;
-    
-    pizzaForm.append(small, smLabel, med, medLabel, lg, lgLabel);
-    menu.append(pizzaForm);
-    chooseToppings();
-}
 
-const chooseToppings = ()=>{
-    const div= document.createElement("div");
-    const title= document.createElement("h3");
-    title.textContent="Choose your Toppings. $1 each";
-    div.append(title)
-    for(let i=0; i<toppings.length; i++){
-        const checkbox= document.createElement("input");
-        checkbox.type="checkbox";
-        const label = document.createElement("label");
-        label.textContent=toppings[i];
-        div.append(checkbox, label);
-    }
-    pizzaForm.append(div)
-    pizzaQuantity();
-}
-const pizzaQuantity = ()=>{
-    const input = document.createElement("input");
-    input.type="number";
-    input.min="1";
-    input.value="1"
+
+// menu buttons
+pizzaMenuButton.addEventListener("click", () => {
+  console.log("clicked");
+  // menu.removeChild(pizzaForm);
+  PizzaSize();
+});
+
+
+// pizza menu
+const PizzaSize = () => {
+  const small = document.createElement("input");
+  small.type = "radio";
+  small.value = 10.99;
+  small.name = "size";
+  const smLabel = document.createElement("label");
+  smLabel.textContent = `Small 10" $10.99`;
+
+  const med = document.createElement("input");
+  med.type = "radio";
+  med.value = 12.99;
+  med.name = "size";
+  const medLabel = document.createElement("label");
+  medLabel.textContent = `Medium 14" $12.99`;
+
+  const lg = document.createElement("input");
+  lg.type = "radio";
+  lg.value = 14.99;
+  lg.name = "size";
+  const lgLabel = document.createElement("label");
+  lgLabel.textContent = `Larger 16" $14.99`;
+
+  pizzaForm.append(small, smLabel, med, medLabel, lg, lgLabel);
+  menu.append(pizzaForm);
+  chooseToppings();
+};
+
+const chooseToppings = () => {
+  const div = document.createElement("div");
+  const title = document.createElement("h3");
+  title.textContent = "Choose your Toppings. $1 each";
+  div.append(title);
+  for (let i = 0; i < toppings.length; i++) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
     const label = document.createElement("label");
-    label.textContent="Number of Pies"
-    pizzaForm.append(input, label);
-    specialDirections();
-}
+    label.textContent = toppings[i];
+    div.append(checkbox, label);
+  }
+  pizzaForm.append(div);
+  pizzaQuantity();
+};
+const pizzaQuantity = () => {
+  const input = document.createElement("input");
+  input.type = "number";
+  input.min = "1";
+  input.value = "1";
+  const label = document.createElement("label");
+  label.textContent = "Number of Pies";
+  label.style.display="block"
+  pizzaForm.append(label, input);
+  specialDirections();
+};
 
-const specialDirections = () =>{
-    const input = document.createElement("input");
-    input.type="textarea";
-    const label=document.createElement("label");
-    label.textContent="Special Directions: ";
-    label.style.display="block";
-    pizzaForm.append(label, input);
-}
-
-PizzaSize();
+const specialDirections = () => {
+  const input = document.createElement("input");
+  input.type = "textarea";
+  const label = document.createElement("label");
+  label.textContent = "Special Directions: ";
+  label.style.display = "block";
+  pizzaForm.append(label, input);
+};
